1a48dcd7b4030b02b0a03349af2b1197350231a3
Ashanti Hatchett <ahatchett02@gmail.com>
2024-10-08 21:20:04 +0000

    added directors model

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,28 @@ Rails.application.routes.draw do
   # CREATE
   post "/movies" => "movies#create", as: :movies
   get "/movies/new" => "movies#new", as: :new_movie
+
+  post "/directors" => "directors#create", as: :directors
+  get "directors/new" => "directors#new", as: :new_director
           
   # READ
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show", as: :movie
+
+  get "/directors" => "directors#index"
+  get "/directors/:id" => "directors#show", as: :director
   
   # UPDATE
   patch "/movies/:id" => "movies#update"
   get "/movies/:id/edit" => "movies#edit", as: :edit_movie
+
+  patch "/directors/:id" => "directors#update"
+  get "/directors/:id/edit" => "directors#edit", as: :edit_director
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
+
+  delete "/directors/:id" => "directors#destroy"
 
   #------------------------------
 end
